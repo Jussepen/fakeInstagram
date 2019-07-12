ac2009bdb38e724b359c2898ce388473f003f494
Jesus Arredondo <jussepen@gmail.com>
2019-07-11 23:16:27 -0700

    Profile picture is changes but not saved (WIP).

--- app/src/main/java/com/example/fakeinstagram/ProfileActivity.java
+++ app/src/main/java/com/example/fakeinstagram/ProfileActivity.java
@@ -1,17 +1,28 @@
 package com.example.fakeinstagram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.parse.ParseFile;
 import com.parse.ParseImageView;
 import com.parse.ParseUser;
+
+import java.io.File;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -19,6 +30,12 @@ public class ProfileActivity extends AppCompatActivity {
     TextView tvUsername;
     ParseImageView ivProfileImage;
     Button btnLogout;
+    Button btnTakeProfilePic;
+    File photoFile;
+
+    public final String TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1000;
+    public String photoFileName = "photo.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +45,9 @@ public class ProfileActivity extends AppCompatActivity {
         ivProfileImage = findViewById(R.id.ivProfileImage);
         tvUsername = findViewById(R.id.tvUsername);
         btnLogout = findViewById(R.id.btnLogout);
+        btnTakeProfilePic = findViewById(R.id.btnTakeProfilePic);
+
+
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +57,15 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnTakeProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLaunchCamera(v);
+            }
+
+        });
+        
 
         tvUsername.setText(ParseUser.getCurrentUser().getUsername());
         ivProfileImage.setParseFile(ParseUser.getCurrentUser().getParseFile("profilePicture"));
@@ -67,6 +96,59 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
     }
+
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+
+
+        photoFile = getPhotoFileUri(photoFileName);
+        ParseFile parseFile = new ParseFile(photoFile);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(ProfileActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            ivProfileImage.setImageBitmap(takenImage);
+
+        }
+    }
+
+
 
 
 }
